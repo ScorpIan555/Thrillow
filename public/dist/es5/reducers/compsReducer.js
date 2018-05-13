2cac60a789f18b683491da7f646503e8605dde02
Ian <ian.daley23@gmail.com>
2018-05-13 16:01:21 -0400

    capturing res/req in compsReducer, made dir for result sets

--- public/dist/es5/reducers/compsReducer.js
+++ public/dist/es5/reducers/compsReducer.js
@@ -10,9 +10,7 @@ var constants = _interopRequire(require("../constants"));
 */
 
 var initialState = {
-  all: null,
-  listingZwid: ""
-};
+  all: null };
 
 module.exports = function (_x, action) {
   var state = arguments[0] === undefined ? initialState : arguments[0];
@@ -24,10 +22,14 @@ module.exports = function (_x, action) {
       console.log("ZILLOW_COMPS_RECEIVED!");
       // Capture request/response objects
       newState.req = payload.body.data.request;
-      // newState['all'] = payload.body.data.response.results.result
+      newState.all = payload.body.data.response.properties;
+      newState.all.principal = payload.body.data.response.properties.principal;
+      newState.all.comparables = payload.body.data.response.properties.comparables;
       // Console log request/response objects
       console.log("compsReducer REQ: " + JSON.stringify(newState.req));
-      // console.log("compsReducer RES: " + JSON.stringify(newState.all))
+      console.log("compsReducer RES: " + JSON.stringify(newState.all));
+      console.log("compsReducer PRINCIPAL: " + JSON.stringify(newState.all.principal));
+      console.log("compsReducer COMPARABLES: " + JSON.stringify(newState.all.comparables));
       // Capture lat/long objects
       // newState.all.latitude = payload.body.data.response.results.result[0].address[0].latitude[0]
       // newState.all.longitude = payload.body.data.response.results.result[0].address[0].longitude[0]
@@ -36,9 +38,15 @@ module.exports = function (_x, action) {
       // console.log("compsReducer LONGITUDE: " + JSON.stringify(newState.all.longitude))
 
 
+
+      // Set up logic for result set, then feed into Listing component
+
+
       return newState;
 
     default:
       return state;
   }
 };
+
+// listingZwid: ''
--- src/reducers/compsReducer.js
+++ src/reducers/compsReducer.js
@@ -7,7 +7,7 @@ import constants from '../constants'
 
 var initialState = {
 	all: null,
-  listingZwid: ''
+  // listingZwid: ''
 }
 
 export default (state = initialState, action) => {
@@ -19,16 +19,24 @@ export default (state = initialState, action) => {
       console.log('ZILLOW_COMPS_RECEIVED!')
       // Capture request/response objects
       newState['req'] = payload.body.data.request
-      // newState['all'] = payload.body.data.response.results.result
+      newState['all'] = payload.body.data.response.properties
+      newState.all.principal = payload.body.data.response.properties.principal
+      newState.all.comparables = payload.body.data.response.properties.comparables
       // Console log request/response objects
       console.log("compsReducer REQ: " + JSON.stringify(newState.req))
-      // console.log("compsReducer RES: " + JSON.stringify(newState.all))
+      console.log("compsReducer RES: " + JSON.stringify(newState.all))
+      console.log("compsReducer PRINCIPAL: " + JSON.stringify(newState.all.principal))
+      console.log("compsReducer COMPARABLES: " + JSON.stringify(newState.all.comparables))
       // Capture lat/long objects
       // newState.all.latitude = payload.body.data.response.results.result[0].address[0].latitude[0]
       // newState.all.longitude = payload.body.data.response.results.result[0].address[0].longitude[0]
       // Console log latitude/longitude objects
       // console.log("compsReducer LATITUDE: " + JSON.stringify(newState.all.latitude))
       // console.log("compsReducer LONGITUDE: " + JSON.stringify(newState.all.longitude))
+
+
+
+      // Set up logic for result set, then feed into Listing component
 
 
       return newState
